254746a3e389b0186f933f35f31a3a9b747fd9ee
Javier Hidalgo <103899360+javihidalgodev@users.noreply.github.com>
2024-03-04 14:10:46 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://javihidalgodev.github.io/portfolio/',
-  base: '/dist'
+  base: 'dist'
 });
