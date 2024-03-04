4efefa24a12db777a8925ee883670b637248bd2d
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2024-03-04 14:28:08 +0100

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://javihidalgodev.github.io',
-  base: '/portfolio'
+  base: '/gh-pages'
 });
