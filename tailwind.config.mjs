3d702a7b7d5b30fec22bb29136afae7c520cdb43
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2024-03-06 19:55:36 +0100

    Technologies Scroll

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -11,5 +14,9 @@ export default {
 	future: {
 		hoverOnlyWhenSupported: true
 	},
-	plugins: [],
+	plugins: [
+		plugin(function({addVariant}){
+			addVariant('children', '&>*')
+		})
+	],
 }
