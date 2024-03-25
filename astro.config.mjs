7f9d503542605cf807cf5056ca2549987257fef2
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2024-03-25 08:29:33 +0100

    Update navbar and fix bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://javihidalgodev.github.io',
-  base: '/',
+  base: '/portfolio',
   output: 'static'
 });
