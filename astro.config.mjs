3e1c1a99e792e2e26c7c03685b4410ef37b10593
Javier Hidalgo <francisco.hidalgo.nula@gmail.com>
2024-04-04 08:43:47 +0200

    Update hostname

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://javihidalgodev.github.io',
+  site: 'https://javierhidalgodev.github.io',
   base: '/portfolio',
   output: 'static'
 });
